e9b3796a5c6a6eb5719b048892a65fd05b5ca282
Sai Rathan <rathansai.bk@gmail.com>
2022-04-14 20:32:45 +0530

    Initial Commiting

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Data;
+using Test.Models;
+using Test.Models.Helper;
+
+namespace Test.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(ApplicationDbContext db, UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager )
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        public ActionResult Login()
+        { 
+             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(Login model)
+        {
+
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+
+                }
+                ModelState.AddModelError("", "Username or password incorrect");
+            }
+            return View(model);
+        }
+        public IActionResult Register()
+        {
+            if(!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+                _roleManager.CreateAsync(new IdentityRole(Helper.User));
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(Register model)
+        {
+           if(ModelState.IsValid)
+            {
+                var usr = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                };
+                var result = await _userManager.CreateAsync(usr,model.Password);
+                if(result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(usr, model.RoleName);
+                    await _signInManager.SignInAsync(usr, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Models;
+
+namespace Test.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        //{
+                
+        //}
+        public DbSet<Login> Logins { get; set; }
+        public DbSet<Register> Registers { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder
+                .Entity<Login>()
+                .Property(e => e.Email)
+                .HasColumnType("varchar(512)");
+            builder
+                .Entity<Login>()
+                .Property(e => e.Password)
+                .HasColumnType("varchar(512)");
+            builder
+                .Entity<Register>()
+                .Property(e => e.Name)
+                .HasColumnType("varchar(512)");
+            builder
+                .Entity<Register>()
+                .Property(e => e.Email)
+                .HasColumnType("varchar(512)");
+            builder
+                .Entity<Register>()
+                .Property(e => e.Password)
+                .HasColumnType("varchar(512)");
+            builder
+                .Entity<Register>()
+                .Property(e => e.ConfirmPassword)
+                .HasColumnType("varchar(512)");
+
+        }
+        protected override void OnConfiguring(
+           DbContextOptionsBuilder optionsBuilder
+       )
+        {
+            optionsBuilder.UseSqlite("Data Source=Sqlitee.db");
+        }
+    }
+    }
