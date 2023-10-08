7ecbb1e0e1a4d2b09f6b6db5882602d6524deac9
Rathanbk <66916691+Rathanbk@users.noreply.github.com>
2023-10-08 12:17:56 +0530

    Add files via upload

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,27 +10,40 @@ namespace Test.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         //{
-                
+
         //}
-        public DbSet<Login> Logins { get; set; }
-        public DbSet<Register> Registers { get; set; }
+        //public DbSet<Login> Logins { get; set; }
+        //public DbSet<Register> Registers { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+       
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             builder
-                .Entity<Login>()
+                     .Entity<Login>()
                 .Property(e => e.Email)
                 .HasColumnType("varchar(512)");
-            builder
-                .Entity<Login>()
+                    builder
+                 .Entity<Login>()
                 .Property(e => e.Password)
                 .HasColumnType("varchar(512)");
             builder
                 .Entity<Register>()
-                .Property(e => e.Name)
+                .Property(e => e.UserName)
                 .HasColumnType("varchar(512)");
+            builder
+                .Entity<Register>()
+                .Property(e => e.FirstName)
+                .HasColumnType("varchar(512)");
+            builder
+               .Entity<Register>()
+               .Property(e => e.LastName)
+               .HasColumnType("varchar(512)");
             builder
                 .Entity<Register>()
                 .Property(e => e.Email)
@@ -43,13 +56,35 @@ namespace Test.Data
                 .Entity<Register>()
                 .Property(e => e.ConfirmPassword)
                 .HasColumnType("varchar(512)");
+            builder
+                .Entity<Item>()
+                .Property(e => e.ItemName)
+                .HasColumnType("varchar(512)");
+            builder
+                .Entity<Item>()
+                .Property(e => e.Borrower)
+                .HasColumnType("varchar(512)");
+            builder
+                .Entity<Item>()
+                .Property(e => e.Lender)
+                .HasColumnType("varchar(512)");
+
+            builder
+                .Entity<Expense>()
+                .Property(e => e.ExpenseName)
+                .HasColumnType("varchar(512)");
+            builder
+                .Entity<Expense>()
+                .Property(e => e.Amount)
+                .HasColumnType("Integer");
+            
 
         }
         protected override void OnConfiguring(
            DbContextOptionsBuilder optionsBuilder
        )
         {
-            optionsBuilder.UseSqlite("Data Source=Sqlitee.db");
+            optionsBuilder.UseSqlite("Data Source=Sqlitee2.db");
         }
     }
     }
